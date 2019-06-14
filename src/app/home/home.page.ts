ba1ce05f190a8d7740b71a6facdf116ca4d2a738
Cleiton Silva Barbosa <cleitonsbarbosa@gmail.com>
2019-06-13 22:36:16 -0300

    Esqueleto Ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,25 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  private relogio = '10:10:15';
+
   constructor() {}
+
+  ngOnInit() {
+    setInterval(this.meuRelogio.bind(this), 1000);
+  }
+
+  meuRelogio() {
+    var d = new Date(), displayDate;
+    if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+      displayDate = d.toLocaleTimeString('pt-BR');
+    } else {
+      displayDate = d.toLocaleTimeString('pt-BR', {timeZone: 'America/Belem'});
+    }
+
+    this.relogio = displayDate;
+  }
+
+
 
 }
